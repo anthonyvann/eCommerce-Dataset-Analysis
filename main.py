311cd9469e3bca837baf626dc46300be1869105d
Anthony Van <avan4@uci.edu>
2023-12-24 19:34:34 -0800

    casted the discount_percentage column as a float

---
+++ main.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def create_dataframe(dataset) -> pd.DataFrame:
+    return pd.read_csv(dataset)
+
+
+def clean_dataset(df: pd.DataFrame) -> pd.DataFrame:
+    """Function to handle missing, duplicate, and incorrect values in the dataset"""
+    df.drop_duplicates(keep='first', inplace=True)  # drop duplicate rows
+    df.dropna(inplace=True)                         # drop rows with missing values
+
+    # Converting Rupee to USD for discounted prices
+    df.discounted_price = df.discounted_price.replace("[₹,]", "", regex=True).astype(float)
+    df.discounted_price = (df.discounted_price * .012).round(2)
+
+    # Converting Rupee to USD for actual prices
+    df.actual_price = df.actual_price.replace("[₹,]", "", regex=True).astype(float)
+    df.actual_price = (df.actual_price * .012).round(2)
+
+    # removing the trailing '%' and converting the value to float dtype
+    df.discount_percentage = df.discount_percentage.str.rstrip("%").astype("float") / 100
+
+    # df = df.astype({
+    #     'rating': float,
+    #     'rating_count': int
+    # })
+
+    return df
+
+
+def get_summary(df: pd.DataFrame) -> pd.DataFrame:
+    """Function to take a dataframe and return the summary statistics for numerical columns."""
+    return df.describe()
+
+
+def main():
+    # Reading the dataset into a dataframe
+    amazon_df = create_dataframe(r'.\amazon.csv')
+
+    # cleaning the dataset
+    clean_dataset(amazon_df)
+
+    # actual_price = amazon_df.actual_price
+    # print(discounted_price.head())
+    # print()
+    # print(actual_price.head())
+
+
+if __name__ == '__main__':
+    main()
